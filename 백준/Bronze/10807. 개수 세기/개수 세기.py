29c5f400af840e1bdfd2e05842c4be40f337197d
JIHYUN <110596178+devtoJH@users.noreply.github.com>
2023-01-15 14:36:47 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,4 +1,5 @@
-N = int(input())
-M = list(map(int, input().split()))
-v = int(input())
+import sys
+N = int(sys.stdin.readline())
+M = list(map(int, sys.stdin.readline().split()))
+v = int(sys.stdin.readline())
 print(M.count(v))
