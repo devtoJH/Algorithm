852257661d523db9a369734025d92415aa6e8cbd
JIHYUN <110596178+devtoJH@users.noreply.github.com>
2023-01-16 15:13:27 +0900

    [Bronze III] Title: 점수계산, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2506. 점수계산/점수계산.py
@@ -0,0 +1,11 @@
+N = int(input())
+score = list(map(int, input().split()))
+cnt = 0
+result = 0
+for i in range(N):
+    if score[i] == 1:
+        cnt += 1
+        result += cnt
+    else:
+        cnt = 0
+print(result)
