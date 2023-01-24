3ef15afbff00fb7cc5050a91a0e8a4da222c1bea
JIHYUN <110596178+devtoJH@users.noreply.github.com>
2023-01-24 14:54:32 +0900

    [Bronze III] Title: ACM 호텔, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,9 @@
+T = int(input())
+for t in range(T):
+    H, W, N = map(int, input().split())
+    num = N//H + 1
+    floor = N % H
+    if N % H == 0:
+        num = N//H
+        floor = H
+    print(floor*100 + num)
