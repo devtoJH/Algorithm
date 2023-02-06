c96b55ae336e62b4d9c6978607122fe23f8d1a16
JIHYUN <110596178+devtoJH@users.noreply.github.com>
2023-02-06 13:52:25 +0900

    [Bronze I] Title: 행복한지 슬픈지, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.py
@@ -0,0 +1,14 @@
+string = input()
+#print(string)
+
+happy = string.count(':-)')
+sad = string.count(':-(')
+
+if happy == 0 and sad == 0:
+    print('none')
+elif happy == sad:
+    print('unsure')
+elif happy > sad:
+    print('happy')
+elif happy < sad:
+    print('sad')
