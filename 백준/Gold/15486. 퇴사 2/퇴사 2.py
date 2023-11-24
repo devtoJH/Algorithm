f243019e45e1d3fa145175f607366e6f7f248202
JIHYUN <qkrwlgus718@gmail.com>
2023-11-24 23:02:57 +0900

    [Gold V] Title: 퇴사 2, Time: 2916 ms, Memory: 168676 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+t = [0]*(n+1)
+p = [0]*(n+1)
+dp = [0]*(n+2)
+
+for i in range(n):
+    t[i+1], p[i+1] = list(map(int, input().split()))
+
+for i in range(n, 0, -1):
+    dp[i] = max(dp[i+1], dp[i])
+    if (i-1) + t[i] <= n:
+        dp[i] = max(dp[i], dp[i + t[i]] + p[i])
+
+print(max(dp))
